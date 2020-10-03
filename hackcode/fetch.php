3ed21f7cfb2abffec346b2456c6ee59afe97ce79
viditj47 <49235539+viditj47@users.noreply.github.com>
2020-10-03 19:41:33 +0530

    Update fetch.php

--- hackcode/fetch.php
+++ hackcode/fetch.php
@@ -24,6 +24,28 @@ while($row = $results->fetch(PDO::FETCH_ASSOC))
                                 <div class="user_img"><img src="profile_images/'.$row['user_image'].'" alt="Profile image"></div>
                                 <div class="user_info"><span>'.$row['username'].'</span>
                                 <span><a href="user_profile.php?id='.$row['id'].' " class="see_profileBtn">See profile</a>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item {% block homeactive  %}{% endblock homeactive %}">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item {% block aboutactive  %}{% endblock aboutactive %}">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+
+             <li class="nav-item {% block blogactive  %}{% endblock blogactive %}">
+              <a class="nav-link" href="/blog">Blog</a>
+            </li>
+
+            
+            <li class="nav-item {% block contactactive  %}{% endblock contactactive %}">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+
+           
+            
+          </ul>
+        </div>
 								
 								
 								</div >
